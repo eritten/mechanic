58db8e23a325081c3c0721cbcddb3cbe905acef2
Klasique <feboapong@gmail.com>
2023-03-27 13:29:15 +0100

    done

--- mechanic/urls.py
+++ mechanic/urls.py
@@ -4,10 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from home import views
+from home.views import home, about
 
 urlpatterns = [
-path('', views.home, name='home'),
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path('about/', about, name='about'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
