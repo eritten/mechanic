ec9069056c11de433690098b29feed1de7427619
= <=>
2023-03-27 11:39:38 -0200

    add changes

--- mechanic/urls.py
+++ mechanic/urls.py
@@ -11,5 +11,4 @@ path('', views.home, name='home'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
