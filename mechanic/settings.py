49b0194e3a9b4ca8a365745dcd60fada9826be44
= <=>
2023-04-01 21:43:13 -0200

    adding hosting features

--- mechanic/settings.py
+++ mechanic/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&s4267#0i=^%!b@-4y+dj(31mndp%+jfe9iliu$=sg%+as2&kk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
